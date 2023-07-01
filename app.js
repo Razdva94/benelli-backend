b0737908cf0e36627f432ce265cec2f09960e263
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-07-01 04:43:51 +0300

    change port

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const { errorHandler } = require('./middlewares/error');
 const app = express();
 app.use(express.json());
 app.use(requestLogger);
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 app.post(
   '/send-info',
@@ -33,6 +27,6 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
